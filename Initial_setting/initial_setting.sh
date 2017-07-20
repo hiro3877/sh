0ed68b79dee7b1bb3a30f6b107ea57c9369cf6a9
hiro3877 <itudemozeroen2@gmail.com>
2017-07-21 05:49:13 +0900

    add initial_setting_sh

---
+++ Initial_setting/initial_setting.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo update-grub
+sudo apt-get install vim tmux ssh sl -y
+
+cd ~
+mkdir projects
+cd projects
+git clone https://github.com/hiro3877/learn-cuda
+git clone https://github.com/hiro3877/learn-cpp
+git clone https://github.com/minaminoki/.dotfiles
+
+git config --global user.name "hiro3877"
+git config --global user.email "itudemozeroen2@gmail.com"
+
+cd .dotfiles
+cp .tmux.conf ~/.tmux.conf
+
